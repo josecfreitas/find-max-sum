64dbf8cde125de207cd8409cb0081d03262b5fe3
José Carlos de Freitas <josecfreitas@outlook.com.br>
2022-12-14 15:22:11 -0300

    answer

--- src/find-max-sum.ts
+++ src/find-max-sum.ts
@@ -1,3 +1,19 @@
 export function findMaxSum(values: number[]) {
-  
+  if (values.length < 2) return 0;
+
+  let maxValue = Number.NEGATIVE_INFINITY;
+  let secondMaxValue = Number.NEGATIVE_INFINITY;
+
+  for (const value of values) {
+    if (secondMaxValue < value) {
+      secondMaxValue = value;
+
+      if (maxValue < value) {
+        secondMaxValue = maxValue;
+        maxValue = value;
+      }
+    }
+  }
+
+  return maxValue + secondMaxValue;
 }
